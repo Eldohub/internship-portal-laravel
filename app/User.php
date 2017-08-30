25b281eee3472fbff4ad7f5a890593f9fdabd84e
Reaper45 <jomwashighadi@gmail.com>
2017-08-30 15:31:16 +0300

    Removed cssloader

--- app/User.php
+++ app/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'password', 'confirmation_code', 'confirmed',
     ];
 
     /**
@@ -25,7 +25,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token',
+        'password', 'remember_token', 'confirmation_code',
     ];
 
     /**
@@ -42,8 +42,14 @@ class User extends Authenticatable
      * @param string $value
      * @return void
      */
-    public function setPasswordAttribute(string $value): void
+    public function setPasswordAttribute(string $value)
     {
         $this->attributes['password'] = bcrypt($value);
     }
+
+    /**
+     *
+     *
+     *
+     */
 }
