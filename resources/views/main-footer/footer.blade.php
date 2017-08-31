c9ca266f0c6530db8e738dbc7e32e116e719fdab
Kevin Barasa <kevin.barasa001@gmail.com>
2017-08-31 03:15:00 +0300

    custom landing page

--- resources/views/main-footer/footer.blade.php
+++ resources/views/main-footer/footer.blade.php
@@ -4,19 +4,17 @@
       <div class="three wide column">
         <h4 class="ui inverted header">About Eldohub</h4>
         <div class="ui inverted link list">
-          <a href="#" class="item">Sitemap</a>
+          <a href="#" class="item">Who We Are</a>
+          <a href="#" class="item">Blog</a>
           <a href="#" class="item">Contact Us</a>
-          <a href="#" class="item">Religious Ceremonies</a>
-          <a href="#" class="item">Gazebo Plans</a>
         </div>
       </div>
       <div class="three wide column">
         <h4 class="ui inverted header">Engage Us for Work</h4>
         <div class="ui inverted link list">
-          <a href="#" class="item">Banana Pre-Order</a>
-          <a href="#" class="item">DNA FAQ</a>
-          <a href="#" class="item">How To Access</a>
-          <a href="#" class="item">Favorite X-Men</a>
+          <a href="#" class="item">Software Engineering</a>
+          <a href="#" class="item">Apps Development</a>
+          <a href="#" class="item">Consultations</a>
         </div>
       </div>
       <div class="seven wide column">
